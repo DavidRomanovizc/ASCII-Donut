5441ccac146b0d0a6128602e64b827efc7e99b1f
David Dzhalaev <72649244+DavidRomanovizc@users.noreply.github.com>
2021-11-14 00:13:22 +0300

    Changed the algorithm

--- Donut.js
+++ Donut.js
@@ -1,34 +1,61 @@
-const pre = document.createElement("pre");
-document.body.appendChild(pre);
+var el = document.getElementById("c");
+var ctx = el.getContext("2d");
+var start = null;
 
-let x = 1760,
-    z = 0, y = 0;
-setInterval
-(() => {
-    z += .07, y += .03;
-    const
-        a = [...new Array(x)].map((a, r
-        ) => r % 80 === 79 ? "\n" : " "), r = new
-        Array(x).fill(0), t = Math.cos(z), e =
-            Math.sin(z), n = Math.cos(y), o = Math.sin
-        (y);
-    for (let s = 0; s < 6.28; s += .07) {
-        const c
-            = Math.cos(s), h = Math.sin(s);
-        for (let
-                 s = 0; s < 6.28; s += .02) {
-            const v = Math
-                .sin(s), M = Math.cos(s), i = c + 2, l = 1
-                / (v * i * e + h * t + 5), p = v * i * t - h * e, d = 0
-                | 40 + 30 * l * (M * i * n - p * o), m = 0 | 12 + 15 * l *
-                (M * i * o + p * n), f = 0 | 8 * ((h * e - v * c * t) * n - v * c *
-                e - h * t - M * c * o), y = d + 80 * m;
-            m < 22 && m >= 0
-            && d >= 0 && d < 79 && l > r[y] && (r[y] = l,
-                a[y] = ".,-~:;=!*#$@"[f > 0 ? f : 0
-                    ])
-        }
+var tmr1 = undefined, tmr2 = undefined;
+var A=1, B=1;
+
+// This is a reimplementation according to my math derivation on the page
+  var R1 = 1;
+  var R2 = 2;
+  var K1 = 150;
+  var K2 = 5;
+  var canvasframe=function() {
+    //var ctx = canvastag.getContext('2d');
+    ctx.fillStyle='#000';
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+    if(tmr1 === undefined) { // only update A and B if the first animation isn't doing it already
+      A += 0.07;
+      B += 0.03;
     }
-    pre.innerHTML = a.join
-    ("")
-}, 50);
+    // precompute cosines and sines of A, B, theta, phi, same as before
+    var cA=Math.cos(A), sA=Math.sin(A),
+        cB=Math.cos(B), sB=Math.sin(B);
+    for(var j=0;j<6.28;j+=0.3) { // j <=> theta
+      var ct=Math.cos(j),st=Math.sin(j); // cosine theta, sine theta
+      for(i=0;i<6.28;i+=0.1) {   // i <=> phi
+        var sp=Math.sin(i),cp=Math.cos(i); // cosine phi, sine phi
+        var ox = R2 + R1*ct, // object x, y = (R2,0,0) + (R1 cos theta, R1 sin theta, 0)
+            oy = R1*st;
+
+        var x = ox*(cB*cp + sA*sB*sp) - oy*cA*sB; // final 3D x coordinate
+        var y = ox*(sB*cp - sA*cB*sp) + oy*cA*cB; // final 3D y
+        var ooz = 1/(K2 + cA*ox*sp + sA*oy); // one over z
+        var xp=(150+K1*ooz*x); // x' = screen space coordinate, translated and scaled to fit our 320x240 canvas element
+        var yp=(120-K1*ooz*y); // y' (it's negative here because in our output, positive y goes down but in our 3D space, positive y goes up)
+        // luminance, scaled back to 0 to 1
+        var L=0.7*(cp*ct*sB - cA*ct*sp - sA*st + cB*(cA*st - ct*sA*sp));
+        if(L > 0) {
+          ctx.fillStyle = 'rgba(255,255,255,'+L+')';
+          //ctx.fillRect(xp, yp, 1.5, 1.5);
+          //ctx.font = 'italic 40pt Calibri';
+          ctx.fillText('+', xp, yp);
+        }
+      }
+    }
+  }
+
+
+function step(ts) {
+  if (!start) {
+    start = ts;
+  }  
+  var dx = ts - start;
+  window.requestAnimationFrame(step);
+  // draw here
+  ctx.clearRect(0,0,300,300);
+  canvasframe();
+}
+
+window.requestAnimationFrame(step);
