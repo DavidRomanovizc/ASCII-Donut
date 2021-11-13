cc6894d79515a51efde657785905c6b864bbed16
David Dzhalaev <72649244+DavidRomanovizc@users.noreply.github.com>
2021-11-13 23:46:32 +0300

    Create Donut.h

---
+++ Donut.h
@@ -0,0 +1,23 @@
+//
+// Created by Romanov on 13.11.2021.
+//
+
+#ifndef DONUT_DONUT_H
+#define DONUT_DONUT_H
+#include <cmath>
+#include <iostream>
+#include <windows.h>
+
+#include <cstdio>
+#include <cmath>
+
+
+void gotoxy(SHORT x, SHORT y) {
+	static HANDLE h = nullptr;
+	if (!h)
+		h = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD c = { x, y };
+	SetConsoleCursorPosition(h, c);
+}
+
+#endif //DONUT_DONUT_H
